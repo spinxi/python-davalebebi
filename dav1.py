77f3d606e8af305e5e7ffb3d24f8a01edab2c805
spinxi <50767516+spinxi@users.noreply.github.com>
2022-03-05 20:38:32 +0400

    Update dav1.py

--- dav1.py
+++ dav1.py
@@ -8,10 +8,12 @@ class Calculator:
         numbers.y = y
     # Create function that makes operations
     def calculateIt(calcNumbers):
-        print('Sum is: ', calcNumbers.x + calcNumbers.y)
-        print('Subtract is: ', calcNumbers.x - calcNumbers.y)
-        print('Multiply is: ', calcNumbers.x * calcNumbers.y)
-        print('Divide is: ', calcNumbers.x / calcNumbers.y)
+        print(
+            'Sum is:', calcNumbers.x + calcNumbers.y,
+            '\nSubtract is:', calcNumbers.x - calcNumbers.y,
+            '\nMultiply is:', calcNumbers.x * calcNumbers.y,
+            '\nDivide is:', calcNumbers.x / calcNumbers.y
+            )
 # Create an empty list for input numbers
 calculatorList = []
 # Create loop for input to run two times
@@ -29,48 +31,71 @@ calculator.calculateIt()
 #OKAY THAT'S IT
 #---------------------------------------------------- </1> ----------------------------------------------------
 
-# #---------------------------------------------------- <2> ----------------------------------------------------
-# #CREATE EMPLOYEE LIST FILTERS & SO ON
-# # Import CSV library for workin' on .csv format files
-# import csv
-# # Create class Calculator
-# class Employee:
-#     # Assign values to object   
-#     def __init__(data, name, surname, age, salary):
-#         data.name = name
-#         data.surname = surname
-#         data.age = age
-#         data.salary = salary
-#     # Create first function
-#     def printingLowest(dataRead):
-#         print('The poorest employer is:', dataRead.name, dataRead.surname, ', Age:', dataRead.age, ', Salary:', dataRead.salary, 'GEL')
-#     # Create first function    
-#     def printingOldest(dataRead):
-#         print('The oldest employer is:', dataRead.name, dataRead.surname, ', Salary:', dataRead.salary, 'GEL')
-# # Let's open our .csv file
-# with open('data.csv') as csvFile:
-#     # Create dictreader object
-#     reader = csv.DictReader(csvFile)
-#     # Create lists to appent infos in here
-#     name = []
-#     surname = []
-#     age = []
-#     salary = []
-#     # Create for loop for reading columns & appending it in .csv file
-#     for col in reader:
-#         name.append(col['name'])
-#         surname.append(col['surname'])
-#         age.append(col['age'])
-#         salary.append(col['salary'])
-# # Let's find out min salary index in list to set the index for every list to take the info
-# indexMinSalary = salary.index(min(salary))
-# minSalary = Employee(name[indexMinSalary], surname[indexMinSalary], age[indexMinSalary], salary[indexMinSalary])
-# # Call the function to print it
-# minSalary.printingLowest()
-# # Let's find out max age index in list to set the index for every list to take the info
-# indexMaxAge = age.index(max(age))
-# maxAge = Employee(name[indexMaxAge], surname[indexMaxAge], age[indexMaxAge], salary[indexMaxAge])
-# # Call the function to print it
-# maxAge.printingOldest()
-# #OKAT THAT'S IT
-# #---------------------------------------------------- </2> ----------------------------------------------------
+
+
+
+#---------------------------------------------------- <2> ----------------------------------------------------
+#CREATE EMPLOYEE LIST FILTERS & SO ON
+# Import CSV library for workin' on .csv format files
+import csv
+# Create class Employee
+class Employee:
+    # Assign values to object   
+    def __init__(data, poorname, poorsurname, poorage, poorsalary, oldname, oldsurname, oldage, oldsalary):
+        # Poor
+        data.poorname = poorname
+        data.poorsurname = poorsurname
+        data.poorage = poorage
+        data.poorsalary = poorsalary
+        # Old
+        data.oldname = oldname
+        data.oldrsurname = oldsurname
+        data.oldage = oldage
+        data.oldsalary = oldsalary
+    # Create first function
+    def printingLowest(dataRead):
+        print(
+            'The poorest employer is:', dataRead.poorname, dataRead.poorsurname, 
+            ', Age:', dataRead.poorage, 
+            ', Salary:', dataRead.poorsalary, 'GEL',
+            '\nThe oldest employer is:', dataRead.oldname, dataRead.oldrsurname,
+            ', Age:', dataRead.oldage,  
+            ', Salary:', dataRead.oldsalary, 'GEL'
+            )
+    # # Create first function    
+    # def printingOldest(dataRead):
+    #     print('The oldest employer is:', dataRead.name, dataRead.surname, ', Salary:', dataRead.salary, 'GEL')
+# Let's open our .csv file
+with open('data.csv') as csvFile:
+    # Create dictreader object
+    reader = csv.DictReader(csvFile)
+    # Create lists to appent infos in here
+    name = []
+    surname = []
+    age = []
+    salary = []
+    # Create for loop for reading columns & appending it in .csv file
+    for col in reader:
+        name.append(col['name'])
+        surname.append(col['surname'])
+        age.append(col['age'])
+        salary.append(col['salary'])
+# Let's find out min & max indexes
+indexMinSalary = salary.index(min(salary))
+indexMaxAge = age.index(max(age))
+minSalary = Employee(
+    # Min salary
+    name[indexMinSalary], 
+    surname[indexMinSalary], 
+    age[indexMinSalary], 
+    salary[indexMinSalary],
+    # Max age
+    name[indexMaxAge], 
+    surname[indexMaxAge], 
+    age[indexMaxAge], 
+    salary[indexMaxAge]
+    )
+# Call the function to print it
+minSalary.printingLowest()
+#OKAT THAT'S IT
+#---------------------------------------------------- </2> ----------------------------------------------------
